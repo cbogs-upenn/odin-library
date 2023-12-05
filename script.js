07ed42dca67ea31e2f35af83f806942602e60a66
Christopher Bogs <cbogs@upenn.edu>
2023-12-05 02:27:56 -0500

    Reconstruct Book constructor

--- script.js
+++ script.js
@@ -2,8 +2,33 @@
 
 const myLibrary = [];
 
-function Book() {
-    //constructor...
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+
+console.log(theHobbit.info());
+
+
+
+function Book(title, author, pageCount, read) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.read = read;
+
+    this.info = function(){
+
+        let readStatus = "";
+
+        if (read){
+            readStatus = "read";            
+        } else {
+            readStatus = "not read yet";
+        }
+ 
+        const bookInfo = title + " by " + author + ", " + pageCount + " pages, " + readStatus;
+
+        return bookInfo;
+    }
+
 }
 
 function addBooktoLibrary(){
